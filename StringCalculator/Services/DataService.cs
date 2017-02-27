b53d12d84ac39826dbf6a383ab7d5cb0bd47d312
Andreea Tiba <a.tiba@levi9.com>
2017-02-27 13:10:11 +0200

    3. Allow the method to have an unknown amount of numbers
    4. The method allows new line as a delimiter: Add("1\n2") should return 3

--- StringCalculator/Services/DataService.cs
+++ StringCalculator/Services/DataService.cs
@@ -4,7 +4,7 @@ namespace StringCalculator.Model
 {
     public class DataService : IDataService
     {
-        private readonly string[] _delimiterChars = { " ", @"\n", Environment.NewLine };
+        private readonly string[] _delimiters = { " ", @"\n", Environment.NewLine };
         public void InitializeData(Action<DataItem, Exception> callback)
         {
             var item = new DataItem(string.Empty, "...");
@@ -16,7 +16,16 @@ namespace StringCalculator.Model
             if (string.IsNullOrEmpty(numbers)) return 0;
             int number;
             if (int.TryParse(numbers, out number)) return number;
+            var stringNumbers = numbers.Split(_delimiters, StringSplitOptions.RemoveEmptyEntries);
             var s = 0;
+            foreach (var sN in stringNumbers)
+            {
+                int currentNumber;
+                if (int.TryParse(sN, out currentNumber))
+                {
+                    s += currentNumber;
+                }
+            }
             return s;
         }
     }
